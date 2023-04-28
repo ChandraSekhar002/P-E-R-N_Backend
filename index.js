f9e7d482fcc5a59820b46b43e10ab9d49c031d01
chandra sekhar <119343229+ChandraSekhar002@users.noreply.github.com>
2023-04-28 11:51:00 +0530

    origin_changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const http = require('http').createServer(app);
 const io = require("socket.io")(http, {
     cors: {
       //origin: "http://localhost:3000",
-      //origin: "*",
+      origin: "*",
       methods: ["GET", "POST"]
     }
   });
